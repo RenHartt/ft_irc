4e60549387c7c7ba1381106f22e3b760dac63d0e
at0m741 <louistouz@icloud.com>
2024-10-22 18:02:06 +0200

    Client

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: bgoron <bgoron@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 00:53:30 by bgoron            #+#    #+#             */
-/*   Updated: 2024/10/22 17:13:37 by babonnet         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:38:18 by babonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,33 @@
 #include <queue>
 #include <set>
 
+
 class Client
 {
 	public:
 		Client();
-		
-	private:
-		/* int						_fd;  */
-		/* int						_port; */
-		std::string				_IP; 
+		Client(const std::string& ip, const std::string& nickname, const std::string& username, const std::string& realname);
+		~Client();
+		const std::string& getIP() const;
+		const std::string& getNickname() const;
+		const std::string& getUsername() const;
+		const std::string& getRealname() const;
 
+		void setIP(const std::string& ip);
+		void setNickname(const std::string& nickname);
+		void setUsername(const std::string& username);
+		void setRealname(const std::string& realname);
+		
+		void setServer(const std::string& server); 
+		const std::string& getServer() const; 
+
+	private:
+		std::string				_IP;
 		std::string				_nickname; 
 		std::string				_username; 
-		std::string				_realname; 
-		
+		std::string				_realname;
 		std::set<std::string>	_channels;
-
 		std::queue<std::string> _private_message;
-
-
+		std::string				_server; 
 };
-
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -6,11 +6,43 @@
 /*   By: bgoron <bgoron@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 01:04:37 by bgoron            #+#    #+#             */
-/*   Updated: 2024/10/22 17:13:24 by babonnet         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:37:46 by babonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Client.hpp"
 
-Client::Client() {}
+Client::Client()
+	: _IP("127.0.0.1"), \
+	_nickname("guest"), \
+	_username("guest"), \
+	_realname("Guest User"), \
+	_server("localhost")
+{
+}
+
+Client::Client(const std::string& ip, \
+			const std::string& nickname, \
+			const std::string& username, \
+			const std::string& realname)
+	: _IP(ip), _nickname(nickname), _username(username), _realname(realname), _server("localhost")
+{
+}
+
+Client::~Client()
+{
+}
+
+const std::string& Client::getIP() const { return _IP; }
+const std::string& Client::getNickname() const { return _nickname; }
+const std::string& Client::getUsername() const { return _username; }
+const std::string& Client::getRealname() const { return _realname; }
+
+void Client::setIP(const std::string& ip) { _IP = ip; }
+void Client::setNickname(const std::string& nickname) { _nickname = nickname; }
+void Client::setUsername(const std::string& username) { _username = username; }
+void Client::setRealname(const std::string& realname) { _realname = realname; }
+
+void Client::setServer(const std::string& server) { _server = server; }
+const std::string& Client::getServer() const { return _server; }
 
