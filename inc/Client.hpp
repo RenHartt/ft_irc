6c1435c602099abfefd95b54d0640c5992dc42e1
bonsthie <barnabe.bonnet@gmail.com>
2024-10-23 20:09:13 +0200

    yes

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -6,15 +6,15 @@
 /*   By: bgoron <bgoron@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 00:53:30 by bgoron            #+#    #+#             */
-/*   Updated: 2024/10/22 20:12:06 by bgoron           ###   ########.fr       */
+/*   Updated: 2024/10/22 22:01:11 by bonsthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <string>
-#include <queue>
 #include <map>
+#include <queue>
+#include <string>
 
 #include "Channel.hpp"
 
@@ -22,22 +22,19 @@ class Channel;
 
 class Client
 {
-	public:
-		Client(int fd);
-		
-		int getFd() const;
-	private:
-		int	_client_fd; 
+      public:
+        Client(int fd);
 
-		std::string	_nickname; 
-		std::string	_username; 
-		std::string	_realname; 
-		
-		std::map<std::string, Channel *>	_channels;
+        int getFd() const;
 
-		std::queue<std::string>	_private_message;
+      private:
+        int _client_fd;
 
+        std::string _nickname;
+        std::string _username;
+        std::string _realname;
 
+        std::map<std::string, Channel *> _channels;
+
+        std::queue<std::string> _private_message;
 };
-
-
