4dd835b494400523a365ecc87b9c12dee1d8f99f
bonsthie <barnabe.bonnet@gmail.com>
2024-10-25 00:10:05 +0200

    woring on Chanel Class add of the permision struct add of the map of permision struct creation of a define to set permision

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,7 +2,40 @@
 
 #include <Client.hpp>
 
+#include <iostream>
 #include <map>
+#include <stdint.h>
+
+struct Rights {
+    uint8_t write : 1;
+    /* uint8_t     uploadImg : 1; */
+
+    struct {
+        uint8_t mute : 1;
+        uint8_t kick : 1;
+        uint8_t ban : 1;
+        uint8_t invite : 1;
+        uint8_t setPassWord : 1;
+        uint8_t changeChannelVisibility : 1;
+        uint8_t changeChannelName : 1;
+    } admin;
+};
+
+struct ClientRight {
+    Rights rights;
+    Rights grantable;
+};
+
+// need to be rework parse the input for better error message
+void err_message(const char *str) { std::cerr << str << std::endl; }
+
+#define SET_PERMISSION(grantor_rights, target_user, permission, state)         \
+    {                                                                          \
+        if (grantor_rights.grantable.permission)                               \
+            target_user.rights.permission = state;                             \
+        else                                                                   \
+            err_message(#permission);                                          \
+    }
 
 class Client;
 
@@ -12,5 +45,5 @@ class Channel
     Channel();
 
   private:
-    std::map<int, Client *> _clients_list;
+    std::map<int, ClientRight> _clients_rights;
 };
