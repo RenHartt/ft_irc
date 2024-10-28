39fe99e2a46c0b5732ea292ad2a2d25a90da3fb8
bonsthie <barnabe.bonnet@gmail.com>
2024-10-28 21:16:37 +0100

    working on generating Permission list at compile time

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -2,58 +2,15 @@
 
 #include <Client.hpp>
 
-#include <iostream>
+#include <Permission.hpp>
 #include <map>
 #include <stdint.h>
-
-struct Rights {
-    uint8_t write : 1;
-    /* uint8_t     uploadImg : 1; */
-
-    struct {
-        uint8_t mute : 1;
-        uint8_t kick : 1;
-        uint8_t ban : 1;
-        uint8_t invite : 1;
-        uint8_t setPassWord : 1;
-        uint8_t changeChannelVisibility : 1;
-        uint8_t changeChannelName : 1;
-        uint8_t changeGrantable : 1;
-    } admin;
-};
-
-struct ClientRight {
-    Rights rights;
-    Rights grantable;
-};
 
 struct ChannelSettings {
     uint8_t inviteOnly : 1;
     uint8_t enableKey : 1;
     uint8_t hiddenMode : 1;
 };
-
-// need to be rework parse the input for better error message
-
-inline void err_message(const char *str) { std::cerr << str << std::endl; }
-
-#define SET_PERMISSION(grantor_rights, target_user, permission, state)         \
-    {                                                                          \
-        if (grantor_rights.grantable.permission)                               \
-            target_user.rights.permission = state;                             \
-        else                                                                   \
-            err_message("rights." #permission);                                \
-    }
-
-#define SET_PERMISSION_GRANTABLE(grantor_rights, target_user, permission,      \
-                                 state)                                        \
-    {                                                                          \
-        if (grantor_rights.grantable.admin.changeGrantable &&                  \
-            grantor_rights.grantable.permission)                               \
-            target_user.grantable.permission = state;                          \
-        else                                                                   \
-            err_message("grantable." #permission);                             \
-    }
 
 class Client;
 
@@ -67,16 +24,19 @@ class Channel
     void broadcastMessage(const std::string &message, Client *sender);
 
     std::string getChannelName(void) const;
-	std::string getPassword(void) const;
+    std::string getPassword(void) const;
     std::string list_channel(Channel *channel) const;
-	
-	bool isMember(Client *client);
+
+    bool isMember(Client *client);
 
   private:
-    std::string                _channel_name;
-    std::string                _password;
-    std::string                _topic;
+    std::string _channel_name;
+    std::string _password;
+    std::string _topic;
 
     std::map<int, ClientRight> _clients_rights;
     ChannelSettings            _channel_settings;
+
+    static MapSetPermission _clients_set_permission_func;
+    static MapSetPermission _set_map_permission(void);
 };
